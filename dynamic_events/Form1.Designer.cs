843c62b95121ad3f2ae309e0a1ba515851714c90
dna-coder <dhope.nagesh@gmail.com>
2016-10-18 13:52:50 -0400

    Basic paint application

---
+++ dynamic_events/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace dynamic_events
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_color_indicator = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(679, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Brush Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_color_indicator
+            // 
+            this.label_color_indicator.Location = new System.Drawing.Point(631, 13);
+            this.label_color_indicator.Name = "label_color_indicator";
+            this.label_color_indicator.Size = new System.Drawing.Size(27, 23);
+            this.label_color_indicator.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 341);
+            this.Controls.Add(this.label_color_indicator);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_color_indicator;
+    }
+}
+
---
+++ dynamic_events/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dynamic_events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label_color_indicator.BackColor = Color.Black;
+            PictureBox picture_box = new PictureBox();
+
+            picture_box.Location = new Point(100, 100);
+            picture_box.BorderStyle = BorderStyle.FixedSingle;
+            picture_box.Size = new Size(500, 500);
+            this.Controls.Add(btn);
+           
+            //register events 
+            btn.MouseDown += new MouseEventHandler(pictureBox_MouseDown);
+            btn.MouseUp += new MouseEventHandler(pictureBox_MouseUp);
+            btn.MouseMove += new MouseEventHandler(pictureBox_MouseMove);
+
+
+        }
+
+        private Point? _Previous = null;
+        private Pen _Pen = new Pen(Color.Black, 10);
+        
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            PictureBox tmpPicBox = (PictureBox)sender;
+            _Previous = new Point(e.X, e.Y);
+           // pictureBox_MouseDown(sender, e);
+        }
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            PictureBox tmpPicBox = (PictureBox)sender;
+            if (_Previous != null)
+            {
+                if (tmpPicBox.Image == null)
+                {
+                    Bitmap bmp = new Bitmap(tmpPicBox.Width, tmpPicBox.Height);
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.Clear(Color.White);
+                    }
+                    tmpPicBox.Image = bmp;
+                }
+                using (Graphics g = Graphics.FromImage(tmpPicBox.Image))
+                {
+                    g.DrawLine(_Pen, _Previous.Value.X, _Previous.Value.Y, e.X, e.Y);
+                }
+                tmpPicBox.Invalidate();
+                _Previous = new Point(e.X, e.Y);
+            }
+
+            
+        }
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            _Previous = null;
+        }
+        private void btn_click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                _Pen = new Pen(colorDialog1.Color, 10);
+                label_color_indicator.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
